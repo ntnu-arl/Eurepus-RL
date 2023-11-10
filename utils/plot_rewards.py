d187f8d60f9dc5870a3dccd7c0380a133f8f3e19
TarekEl-Agroudi <tarek.elagroudi@gmail.com>
2023-11-10 16:48:10 +0100

    Plot TB summaries as PGF for Latex compatibility

--- utils/plot_rewards.py
+++ utils/plot_rewards.py
@@ -1,12 +1,37 @@
 from tensorboard.backend.event_processing import event_accumulator
+import matplotlib
+
 import matplotlib.pyplot as plt
+matplotlib.use("pgf")
+plt.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
 
 # Load TensorBoard event file
-log_path = '../runs/Olympus/summaries'
+log_path = '../Event_Files/2D_sym_colls'
 event_acc = event_accumulator.EventAccumulator(log_path)
 event_acc.Reload()
 
 # Specify the tags for the desired rewards/metrics
+
+data_to_plot = {
+    'detailed_rewards/orient/iter' : ['Orientation reward','Batch reward sum'], 
+    'detailed_rewards/is_done/iter' : ['Terminal reward','Batch reward sum'], 
+    'detailed_rewards/is_within_threshold/iter' : ['Within threshold reward','Batch reward sum'], 
+    'detailed_rewards/torque_clip/iter' : ['Torque clip reward','Batch reward sum'],
+    'detailed_rewards/action_clip/iter' : ['Action clip reward','Batch reward sum'],
+    'detailed_rewards/collision/iter' : ['Collision reward','Batch reward sum'],
+    'losses/a_loss' : ['Actor loss','loss'],
+    'losses/bounds_loss' : ['Bounds loss','loss'],
+    'losses/c_loss' : ['Critic Loss','loss'],
+    'losses/entropy' : ['Entropy','entropy'],
+    'episode_lengths/iter' : ['Episode lengths','Episode length'],
+    'rewards/iter'  : ['Total generalized reward','Reward']
+}
+
 tags_to_plot = [
     'detailed_rewards/orient/iter', 
     'detailed_rewards/is_done/iter', 
@@ -22,10 +47,10 @@ tags_to_plot = [
     'rewards/iter'
     ]
 
-for tag in tags_to_plot:
+for tag in data_to_plot.keys():
     try:
         # Create a new plot for each tag
-        plt.figure(figsize=(10, 5))
+        plt.figure(figsize=(6, 2.5))
         
         reward_events = event_acc.Scalars(tag)
         steps = [event.step for event in reward_events]
@@ -35,12 +60,14 @@ for tag in tags_to_plot:
         plt.plot(steps, rewards)
         
         # Set title and labels
-        plt.title(tag.replace('detailed_rewards/', '').replace('/iter', ''))
-        plt.xlabel('Steps')
-        plt.ylabel('Reward')
+        plt.title(data_to_plot[tag][0], fontsize=10)
+        # plt.xlabel('Epochs', fontsize=10)
+        plt.ylabel(data_to_plot[tag][1], fontsize=10)
 
         # Save the plot as a PDF. NB: DO NOT USE EPS
-        plt.savefig(f'../plots/{tag.replace("/", "_")}_plot.pdf', format='pdf')
+        plt.savefig(f'../plots/pgf/{tag.replace("/", "_")}_plot.pgf', format='pgf')
+        plt.savefig(f'../plots/pdf/{tag.replace("/", "_")}_plot.pdf', format='pdf')
+
         
     except KeyError as e:
         print(f"KeyError: {e} - Skipping tag: {tag}")
